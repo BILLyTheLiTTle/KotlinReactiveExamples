ef0fde6598b8d31b0c6f648987c372e923a0712e
Bill Tsapalos <littleprog@gmail.com>
2019-01-01 15:48:18 +0200

    Create a simple Observable

---
+++ src/main/kotlin/simpleCustomObservable/ObservableCreate.kt
@@ -0,0 +1,36 @@
+package simpleCustomObservable
+
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+
+fun main(args: Array<String>) {
+    val observer = object: Observer<String>{
+        override fun onComplete() {
+            println("Completed")
+        }
+
+        override fun onNext(item: String) {
+            println("Key: $item")
+        }
+
+        override fun onError(e: Throwable) {
+            println("Error ${e.message}")
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            println("New Subscription ")
+        }
+    }
+
+    val observable = Observable.create<String> {
+        var key = readLine()
+        while(key != "0") {
+            it.onNext(key?:"no character")
+            key = readLine()
+        }
+        it.onComplete()
+    }
+
+    observable.subscribe(observer)
+}
