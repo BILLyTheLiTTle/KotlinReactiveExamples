0a63f94ea0d41665cbdd62cf1f0c44368cf25299
Vasilios Tsapalos <littleprog@gmail.com>
2019-01-16 10:57:02 +0200

    Nifty Callable Example

--- src/main/kotlin/concurrencyNparallelism/ThreadAssignment.kt
+++ src/main/kotlin/concurrencyNparallelism/ThreadAssignment.kt
@@ -3,6 +3,8 @@ package concurrencyNparallelism
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 
+// For something nifty about thread assignment @see observableObserverSubject.ObservableFrom.kt, the Callable example
+
 fun main(args: Array<String>) {
     val observable = Observable.range(1,10)
 
@@ -11,22 +13,20 @@ fun main(args: Array<String>) {
         // for every below operator observe in io thread
         //.observeOn(Schedulers.io()) // comment - uncomment
         .map {
-            val item = "Thread (io): ${Thread.currentThread()}\nValue: $it\n"
-            println(item)
-            item
+            println("Thread (io): ${Thread.currentThread()}\n")
+            it
         }
         // for every below operator observe in computation thread
-        //.observeOn(Schedulers.computation()) // comment - uncomment
+        .observeOn(Schedulers.computation()) // comment - uncomment
         .map {
-            val item = "Thread (computation): ${Thread.currentThread()}\nValue: $it\n"
-            println(item)
-            item
+            println("Thread (computation): ${Thread.currentThread()}\n")
+            it
         }.map {
-            val item = "Thread (computation again): ${Thread.currentThread()}\nValue: $it\n"
-            println(item)
-            item
+            println("Thread (computation again): ${Thread.currentThread()}\n")
+            it
         }
         // subscribe and observe in new thread if no observeOn operator has not set up something already
+        // subscribeOn() seems to work like it is the first thread assignment operator no matter where it is written
         .subscribeOn(Schedulers.newThread()) // comment - uncomment
         .subscribe{ println("<-- Item Arrived on ${Thread.currentThread()} -->\n$it") }
 
--- src/main/kotlin/observableObserverSubject/ObservableFrom.kt
+++ src/main/kotlin/observableObserverSubject/ObservableFrom.kt
@@ -3,6 +3,9 @@ package observableObserverSubject
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
 
 fun main(args: Array<String>) {
     val observer = object: Observer<String> {
@@ -29,4 +32,30 @@ fun main(args: Array<String>) {
 
     observable.subscribe(observer)
 
+    // -- Simple example with Callable --
+    println("\n<-- Simple Callable experiments as a small introduction! -->")
+    // run in the main thread
+    println("Direct call to the call() function: ${MyCallable().call()}")
+    // run in a different thread
+    val service = Executors.newFixedThreadPool(5)
+    println("Call from a Thread the call() function: ${service.submit(MyCallable()).get()}")
+    service.shutdown()
+
+    // -- Reactive example with Callable --
+    println("\n<-- Reactive Callable experiments as a small introduction! -->")
+    // Comment - uncomment the lines below to see the thread in which the onNext() and Callable() are running
+    Observable.fromCallable(MyCallable())
+        // affects the thread where the following operators are running
+        //.observeOn(Schedulers.io()) // Comment - uncomment
+        // affects the thread where the callable is running and all the following operators
+        .subscribeOn(Schedulers.newThread()) // Comment - uncomment
+        .subscribe { println("Callable Thread name: $it\nObserver Thread name: ${Thread.currentThread()}")}
+    // For a better explanation please @see concurrencyNparallelism.ThreadAssignment.kt
+
+
+    Thread.sleep(1000)
+}
+
+class MyCallable: Callable<String> {
+    override fun call(): String = Thread.currentThread().toString()
 }
